fdd509db280178fe8f40cd5c5af79342c7b7f444
Chiwon <chiwon99881@gmail.com>
2019-07-08 12:48:40 +0900

    basic Setting

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,5 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+const AuthNavigation = createStackNavigator();
+
+export default createAppContainer(AuthNavigation);
---
+++ styles.js
@@ -0,0 +1,10 @@
+export default {
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  superLightGreyColor: "#dfe6e9",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  greyColor: "#F9F9F9"
+};
