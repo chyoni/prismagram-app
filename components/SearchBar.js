c539ddcbbce18ef19bf30cdfcc690eed6f3a38a8
Chiwon <chiwon99881@gmail.com>
2019-07-10 00:26:29 +0900

    Search Part 2

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { TextInput } from "react-native";
 import PropTypes from "prop-types";
+import styles from "../styles";
+import constants from "../constants";
 
 const SearchBar = ({ onChange, value, onSubmit }) => {
   return (
     <TextInput
+      style={{
+        width: constants.width - 40,
+        height: 35,
+        backgroundColor: "#ecf0f1",
+        borderRadius: 15,
+        padding: 5,
+        fontSize: 13
+      }}
       value={value}
-      onChange={onChange}
-      onSubmit={onSubmit}
-      placeholder={"Search"}
+      onChangeText={onChange}
+      returnKeyType={"search"}
+      onSubmitEditing={onSubmit}
+      placeholder={`🔍 Search`}
+      placeholderTextColor={styles.darkGreyColor}
     />
   );
 };
--- screens/Tabs/Search.js
+++ screens/Tabs/Search.js
@@ -13,9 +13,36 @@ const Text = styled.Text``;
 export default class extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     headerTitle: (
-      <SearchBar value={""} onChange={() => null} onSubmit={() => null} />
+      <SearchBar
+        value={navigation.getParam("term", "")}
+        onChange={navigation.getParam("onChange", () => null)}
+        onSubmit={navigation.getParam("onSubmit", () => null)}
+      />
     )
   });
+  constructor(props) {
+    super(props);
+    const { navigation } = props;
+    this.state = {
+      term: ""
+    };
+    navigation.setParams({
+      term: this.state.term,
+      onChange: this.onChange,
+      onSubmit: this.onSubmit
+    });
+  }
+
+  onChange = text => {
+    const { navigation } = this.props;
+    this.setState({ term: text });
+    navigation.setParams({
+      term: text
+    });
+  };
+  onSubmit = () => {
+    console.log("Submit");
+  };
 
   render() {
     return (
