16be40782e1b4c935e7afd83ba6dc19ff55fedaa
Chiwon <chiwon99881@gmail.com>
2019-07-11 13:37:57 +0900

    Uplaod Photo Part 2 with Permmissions

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components";
+import Loader from "../../components/Loader";
 
 const View = styled.View`
   justify-content: center;
@@ -9,8 +12,31 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default ({ navigation }) => (
-  <View>
-    <Text>Select Photo</Text>
-  </View>
-);
+export default ({ navigation }) => {
+  const [loading, setLoading] = useState(true);
+  const [hasPermission, setHasPermission] = useState(false);
+
+  const askPermission = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        setHasPermission(true);
+      }
+    } catch (e) {
+      console.log(e);
+      setHasPermission(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    askPermission();
+  }, []);
+
+  return (
+    <View>
+      {loading ? <Loader /> : <Text>{hasPermission ? "Thanks" : "Opps"}</Text>}
+    </View>
+  );
+};
